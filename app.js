2bcadeef37b7d0b8f50d72c5c0271d3ce9f28448
Brad Horrocks <bradhorrocks@attask.com>
2013-04-14 21:53:41 -0600

    fixing app

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 
-
-
-
-var app = require('./scheduler');
-app.listen(3001);
+var app = require('./index');
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.port, app.settings.env);
